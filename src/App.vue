7c3c5c02da6440647cc04781451eee2a3ce48f96
Willy Sihombing <willysihombing6@gmail.com>
2023-01-10 01:01:33 +0700

    set up component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <LearningResources />
+</template>
+<script>
+import LearningResources from '@/components/learning-resource/LearningResources.vue';
+export default {
+  components: {
+    LearningResources,
+  },
+  data() {
+    return {
+      storedMessages: [
+        {
+          id: 'Official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
